889c1f206bfe3c0dd17407ef7e8ffc33c8fda060
Kartikey bhatnagar <kartikeybhatnagar247@gmail.com>
2024-11-10 16:55:56 +0000

    intial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings:["Frontend developer","2nd year undergrad","Web Developer"],
+    typeSpeed:100,
+    backspeed:100,
+    backDelay:1000,
+    loop:true
+  });
+  
